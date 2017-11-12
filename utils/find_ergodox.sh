ac440fa9ef06b00ded3169c3b8b7cd1f7cb1eba7
Odd <oddweb42+gith@gmail.com>
2017-11-12 13:57:02 +0100

    find_ergodox: also search for teensy bootloader
    * when the ergodox is in flash mode (after you've pressed the reset
      button), it registers as a teensy halfkay bootloader. Might
      be useful to know its address.

--- utils/find_ergodox.sh
+++ utils/find_ergodox.sh
@@ -2,6 +2,7 @@
 
 root_path=/dev/bus/usb
 model_str="ErgoDox"
+teensy_str="Teensy"
 #ls $root_path
 for port in `ls $root_path`; do
     port_path="$root_path/$port"
@@ -9,7 +10,12 @@ for port in `ls $root_path`; do
         dev_path="$port_path/$dev"
         result=`udevadm info $dev_path | ag -i $model_str`
         if [ $? -eq 0 ]; then
+            # ag returns 0 if it finds a match
             echo "Found an ErgoDox device at: $dev_path"
+        fi
+        result=`udevadm info $dev_path | ag -i $teensy_str`
+        if [ $? -eq 0 ]; then
+            echo "Found a teensy halfkay bootloader at: $dev_path"
         fi
     done
 done
