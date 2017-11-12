1d6ed332308f3bdfbe1ff4f155d6cc51c21b07e7
Odd <oddweb42+gith@gmail.com>
2017-11-12 14:25:14 +0100

    fix mouse sensitivity for v11

--- ergodox_ez/config.h
+++ ergodox_ez/config.h
@@ -33,12 +33,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 14
 #define MATRIX_COLS 6
 
-#define MOUSEKEY_INTERVAL       20
-#define MOUSEKEY_DELAY          0
-#define MOUSEKEY_TIME_TO_MAX    0
-#define MOUSEKEY_MAX_SPEED      7
-#define MOUSEKEY_WHEEL_DELAY 0
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 0
+#define MOUSEKEY_INTERVAL          20
+#define MOUSEKEY_DELAY             0
+#define MOUSEKEY_TIME_TO_MAX       7
+#define MOUSEKEY_MAX_SPEED         5
+#define MOUSEKEY_WHEEL_DELAY       0
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 7
 
 #define TAPPING_TOGGLE  1
 
