fe8b705292489f840c049f7a30ba6b86e994147b
Odd <oddweb42+gith@gmail.com>
2017-11-20 08:56:25 +0100

    move mouse settings to layout-specific config.h

--- ergodox_ez/keymaps/odd_v11/config.h
+++ ergodox_ez/keymaps/odd_v11/config.h
@@ -1,1 +1,8 @@
 #include "../../config.h"
+
+#define MOUSEKEY_INTERVAL          20
+#define MOUSEKEY_DELAY             0
+#define MOUSEKEY_TIME_TO_MAX       10
+#define MOUSEKEY_MAX_SPEED         5
+#define MOUSEKEY_WHEEL_DELAY       0
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 10
